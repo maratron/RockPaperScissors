75038e55bf0875d1c5bef8ff36f44e7b54a75847
Nico Hämäläinen <nico@sizeof.io>
2016-03-17 11:54:58 +0200

    Initial Commit

---
+++ RockPaperScissors/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RockPaperScissors
+//
+//  Created by Nico Hämäläinen on 17/03/16.
+//  Copyright © 2016 sizeof.io. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
