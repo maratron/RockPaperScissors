0a9e53d901d787d6fed155f26d8c9bc35ae8a842
Nico Hämäläinen <nico@sizeof.io>
2016-03-17 14:14:27 +0200

    Added Choice Enum

--- RockPaperScissors/main.swift
+++ RockPaperScissors/main.swift
@@ -5,8 +5,24 @@
 //  Created by Nico Hämäläinen on 17/03/16.
 //  Copyright © 2016 sizeof.io. All rights reserved.
 //
-
 import Foundation
+import GameplayKit
 
-print("Hello, World!")
+// MARK: - Possible Choices
 
+enum Choice: Int {
+  case Rock
+  case Paper
+  case Scissors
+  
+  /// The string representation of the choice value
+  var description: String {
+    switch self {
+    case .Rock: return "Rock"
+    case .Paper: return "Paper"
+    case .Scissors: return "Scissors"
+    }
+  }
+}
+
+print(Choice.Rock)
