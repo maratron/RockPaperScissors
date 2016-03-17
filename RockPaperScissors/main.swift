9f00bd71befc1b8e836c0562765890eb47040ef1
Nico Hämäläinen <nico@sizeof.io>
2016-03-17 14:32:12 +0200

    Final game function

--- RockPaperScissors/main.swift
+++ RockPaperScissors/main.swift
@@ -86,10 +86,54 @@ func getUserInputChoice() -> Choice? {
   return Choice(string: getUserInput())
 }
 
-print("Pick one (R/P/S):")
-if let choice = getUserInputChoice() {
-  print("You picked \(choice).")
+// MARK: - Main Game
+
+func start() {
+  print("Welcome to Rock Paper Scissors!")
+  
+  // Best of 3
+  let maximumRounds = 3
+  var currentRounds = 0
+  
+  // How many wins?
+  var wins = 0
+  
+  while currentRounds < maximumRounds {
+    print("Pick one: R, P, S")
+    if let choice = getUserInputChoice() {
+      let opposingChoice = Choice.random()
+      
+      print("You picked \(choice)")
+      print("Opponent picked \(opposingChoice)")
+
+      // Skip the score checking if it's a draw
+      guard choice != opposingChoice else {
+        print("It's a draw!")
+        continue
+      }
+
+      // Check for player win
+      if choice.beats(opposingChoice) {
+        print("Your \(choice) beats the opponent's \(opposingChoice)!")
+        wins += 1
+      }
+      else {
+        print("The opponent's \(opposingChoice) beats your \(choice)!")
+      }
+      
+      // Increase round counter
+      currentRounds += 1
+    }
+  }
+  
+  if wins >= 2 {
+    print("You won \(wins)-\(maximumRounds-wins)!")
+  }
+  else {
+    print("You lost \(maximumRounds-wins)-\(wins)!")
+  }
+  
+  print("Thanks for playing!")
 }
-else {
-  print("Invalid choice.")
-}
+
+start()
